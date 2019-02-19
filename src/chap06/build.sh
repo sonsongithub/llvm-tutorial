b9ce594054ac6e68c2d8d9733267fa662d431a9b
sonson <yoshida.yuichi@gmail.com>
2019-02-19 19:03:50 +0900

    cmakeとbuildスクリプトをアップデート．

--- src/chap06/build.sh
+++ src/chap06/build.sh
@@ -1,2 +1,3 @@
-clang++ -c ./main.cpp -o ./main.o `llvm-config --cxxflags`
-clang++ -o ./a.out ./main.o `llvm-config --ldflags --libs --libfiles --system-libs core orcjit native`
+LLVM_CONFIG="<path to llvm-config>"
+clang++ -c ./main.cpp -o ./main.o `${LLVM_CONFIG} --cxxflags`
+clang++ -o ./a.out ./main.o `${LLVM_CONFIG} --ldflags --libs --libfiles --system-libs`
--- src/chap07/build.sh
+++ src/chap07/build.sh
@@ -1,2 +1,3 @@
-clang++ --std=c++14 -c ./main.cpp -o ./main.o `llvm-config --cxxflags`
-clang++ --std=c++14 -o ./a.out ./main.o `llvm-config --ldflags --libs --libfiles --system-libs core orcjit mcjit native`
+LLVM_CONFIG="<path to llvm-config>"
+clang++ -c ./main.cpp -o ./main.o `${LLVM_CONFIG} --cxxflags`
+clang++ -o ./a.out ./main.o `${LLVM_CONFIG} --ldflags --libs --libfiles --system-libs`
